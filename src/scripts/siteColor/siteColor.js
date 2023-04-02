6dfdf4309e171c571951166b278a2a598e52158c
Mark Bordakov <mark12bord@gmail.com>
2023-04-02 18:04:08 +0300

    Change Home page design

--- src/scripts/siteColor/siteColor.js
+++ src/scripts/siteColor/siteColor.js
@@ -33,18 +33,22 @@ export function getAndSetColor() {
 }
 
 export function siteColor() {
+    const root = document.getElementById('root');
+    const body = document.querySelector('body');
     const windowError = document.getElementById('window_error');
     const selectDataBases = document.getElementById('select_data_bases');
     const dataBasesSection = document.querySelectorAll('.data_bases_section');
 
     if (document.getElementById('input_in_sites_color').checked===false) {
+        body.style.background = '#F5F5F5';
+        root.classList.remove('root_dark');
         if (windowError) windowError.classList.remove('window_error_dark');
-        document.querySelector('body').style.background = '#F5F5F5';
         if (selectDataBases) selectDataBases.classList.remove('select_data_bases_dark');
         dataBasesSection.forEach((elem) => elem?.classList.remove('data_bases_section_dark'));
     } else {
+        body.style.background = '#595959';
+        root.classList.add('root_dark');
         if (windowError) windowError.classList.add('window_error_dark');
-        document.querySelector('body').style.background = '#595959';
         if (selectDataBases) selectDataBases.classList.add('select_data_bases_dark');
         dataBasesSection.forEach((elem) => elem?.classList.add('data_bases_section_dark'));
     }
